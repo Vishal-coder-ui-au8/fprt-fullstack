2dad6b3ab97825995eaf76fd05175ddc7c5b78c6
Vishal sreevaatsav <vishalvatsav@gmail.com>
2021-02-16 23:08:10 +0530

    commit

---
+++ backend/routes/userRouter.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const userController = require('../controllers/userController')
+const auth = require('../middleware/auth')
+
+router.post('/register', userController.register)
+
+router.post('/login', userController.login)
+
+router.get('/logout', userController.logout)
+
+router.get('/refresh_token', userController.refreshToken)
+
+router.get("/user_details", auth, userController.getUser )
+
+
+//First postman testing user register
+
+// router.post('/register', (req,res) =>{
+//     res.json({msg: "Test Router"})
+// })
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileUpload')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+//routes
+app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
+
+// Connection to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err =>{
+    if(err) throw err;
+    console.log('Connection to MongoDB success')
+})
+
+//first postman testing
+
+// app.get('/', (req, res) =>{
+//     res.json({msg: "Welcome"})
+// })
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () =>{
+    console.log('Server is running on port', PORT)
+})
